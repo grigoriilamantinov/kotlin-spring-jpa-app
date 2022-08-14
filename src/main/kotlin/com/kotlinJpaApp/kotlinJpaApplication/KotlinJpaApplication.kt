92bcfb4024de8bc34f42c80ec14766890d237ffa
grigorii <grigoriiborodin@yandex.ru>
2022-08-15 01:02:05 +0300

    clean up code

--- src/main/kotlin/com/kotlinJpaApp/kotlinJpaApplication/KotlinJpaApplication.kt
+++ src/main/kotlin/com/kotlinJpaApp/kotlinJpaApplication/KotlinJpaApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinCrudApplication
+class KotlinJpaApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinCrudApplication>(*args)
+    runApplication<KotlinJpaApplication>(*args)
 }
